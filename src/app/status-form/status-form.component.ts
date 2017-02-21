9bc494418f883ad590e60c8426327599e8ef1528
nathan-j-brenner <nathan.j.brenner@gmail.com>
2017-02-21 07:20:12 -0800

    drafted form

--- src/app/status-form/status-form.component.ts
+++ src/app/status-form/status-form.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Child } from './child.model';
 @Component({
   selector: 'app-status-form',
   templateUrl: './status-form.component.html',
   styleUrls: ['./status-form.component.css']
 })
 export class StatusFormComponent implements OnInit {
+  child = new Child('', '');
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(){
+    console.log(this.child);
   }
 
 }
