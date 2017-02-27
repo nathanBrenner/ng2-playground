d2fe5b121d34cfec7ac9993154f365b6864d782e
nathan-j-brenner <nathan.j.brenner@gmail.com>
2017-02-26 16:36:39 -0800

    routerLink

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
+<nav>
+  <a routerLink="">Home</a>
+  <a routerLink="contacts">Contacts</a>
+</nav>
 <router-outlet></router-outlet>`,
   styleUrls: ['./app.component.css']
 })
